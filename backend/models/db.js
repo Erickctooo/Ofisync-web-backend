94516da66352b0b00bc8ba81343e42cd008c7399
Erick San Martin <erick20sanmartin.z@gmail.com>
2025-10-17 15:22:35 -0300

    commit simplet

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.PG_USER || "postgres",
-  host: process.env.PG_HOST || "localhost",
-  database: process.env.PG_DATABASE || "ofisync",
-  password: process.env.PG_PASSWORD || "1234",
-  port: process.env.PG_PORT || 5432,
+  user: process.env.PG_USER ,
+  host: process.env.PG_HOST ,
+  database: process.env.PG_DATABASE ,
+  password: process.env.PG_PASSWORD ,
+  port: process.env.PG_PORT 
 });
 
 pool.connect()
